7c98b757ac86b12efd2a26f6a43237d66f75ccab
Zombony <ilya_77797@mail.ru>
2018-06-25 18:53:07 +0600

    Create app2.js

---
+++ getters_-_setters/app2.js
@@ -0,0 +1,25 @@
+function CoffeeMachine(power, capacity) {
+  //...
+  this.setWaterAmount = function(amount) {
+    if (amount < 0) {
+      throw new Error("Значение должно быть положительным");
+    }
+    if (amount > capacity) {
+      throw new Error("Нельзя залить воды больше, чем " + capacity);
+    }
+
+    waterAmount = amount;
+  };
+
+  this.getWaterAmount = function() {
+    return waterAmount;
+  };
+
+
+  this.getPower = function(){
+  	return power;
+  }
+}
+
+var cm = new CoffeeMachine('600','500');
+alert(cm.getPower());
