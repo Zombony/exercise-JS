f075f83f7694b79468348993d4b092761c59b060
Zombony <ilya_77797@mail.ru>
2018-06-27 19:18:36 +0600

    Create app1.js

---
+++ prototype object/app1.js
@@ -0,0 +1,24 @@
+var head = {
+  glasses: 1
+};
+
+var table = {
+  pen: 3
+};
+
+var bed = {
+  sheet: 1,
+  pillow: 2
+};
+
+var pockets = {
+  money: 2000
+};
+
+pockets.__proto__ = bed;
+bed.__proto__ = table;
+table.__proto__ = head;
+
+alert(pockets.pen);
+alert(bed.glasses);
+alert(table.money);
